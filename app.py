c7b021b435eeae1efb0de82f41a327e8350a820e
Misha <slip2005hak@gmail.com>
2023-02-25 16:15:53 +0200

    fix

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 
 @app.route('/likes')
 def get_likes():
-    list_names = ["Андрей", "Жанна", "Катя", "Настя", "Алекс", "Миша"]
+    list_names = ["Андрей","Жена","Степан", "Dima"]
+    dict_answer ={
+        "error": False,
+        "data": '',
+        "error_message": None
+    }
+
     if any(char.isdigit() or not char.isalpha() for char in list_names) or len(list_names) > 10:
         return {
             "error": True,
@@ -14,12 +20,16 @@ def get_likes():
             "error_message": "Ошибки в имени пользователя"
         }
 
-    return {
-        "error": False,
-        "data": f"{list_names[0]}, {list_names[1]} и ещё {len(list_names[1:])} человека лайкнули это",
-        "error_message": None
-    }
-
-
+    if len(list_names) == 0:
+        dict_answer['data'] = 'Это никому не нравится'
+    elif len(list_names) == 1:
+        dict_answer['data'] = f'{list_names[0]} лайкнул это'
+    elif len(list_names) == 2:
+        dict_answer['data'] = f'{list_names[0]} и {list_names[1]} лайкнули это'
+    elif len(list_names) == 3:
+        dict_answer['data'] = f'{", ".join(str(item) for item in list_names[0:2])} и {list_names[2]} лайкнули это'
+    else:
+        dict_answer['data'] = f'{", ".join(str(item) for item in list_names[0:2])} и еще {len(list_names[2:])} лайкнули это'
+    return dict_answer
 if __name__ == '__main__':
     app.run()
